cc39e49588e3871ed72c5f2d9cea49defec030ec
Odimac84 <bjorn.nilsson1984@gmail.com>
2021-09-26 22:03:06 +0000

    commit

---
+++ scripts/game.js
@@ -0,0 +1,26 @@
+let game = {
+    currentGame: [],
+    score: 0,
+    playerMoves: [],
+    choices: ["button1", "button2", "button3", "button4"],
+};
+
+function newGame() {
+    game.score = 0;
+    playerMoves = [];
+    currentGame = [];
+    showScore();
+    addTurn();
+}
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    //showTurns;
+}
+
+module.exports = { game, newGame, showScore, addTurn };
